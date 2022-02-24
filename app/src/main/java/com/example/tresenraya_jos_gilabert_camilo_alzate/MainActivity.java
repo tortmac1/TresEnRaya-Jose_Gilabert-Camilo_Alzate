0652bf27c042fadfeeb00f5e52b01d95c2065726
tortmac <tortmacdelaletra@gmail.com>
2022-02-24 18:07:06 +0100

    Inicio del proyecto Tres en Raya, creación del repositorio

---
+++ app/src/main/java/com/example/tresenraya_jos_gilabert_camilo_alzate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tresenraya_jos_gilabert_camilo_alzate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
