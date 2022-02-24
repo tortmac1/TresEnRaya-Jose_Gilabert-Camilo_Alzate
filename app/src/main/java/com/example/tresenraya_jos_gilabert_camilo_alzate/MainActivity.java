c72128d05ea02d88ecea9ffc18e213e5f12f59a7
josegiga <jose.gilabert@students.salle.url.edu>
2022-02-24 19:00:07 +0100

    MainActivity primera actualización

--- app/src/main/java/com/example/tresenraya_jos_gilabert_camilo_alzate/MainActivity.java
+++ app/src/main/java/com/example/tresenraya_jos_gilabert_camilo_alzate/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.tresenraya_jos_gilabert_camilo_alzate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textoVictoria;
+    Integer[] botones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoVictoria = (TextView) findViewById(R.id.textoVictoria);
+        textoVictoria.setVisibility(View.INVISIBLE);
+
+        botones  = new Integer[]{
+                R.id.b1, R.id.b2, R.id.b3,
+                R.id.b4, R.id.b5, R.id.b6,
+                R.id.b7, R.id.b8, R.id.b9,
+        };
+    }
+
+    public void ponerFicha(View v){
+        int numBoton = Arrays.asList(botones).indexOf(v.getId());
     }
 }
