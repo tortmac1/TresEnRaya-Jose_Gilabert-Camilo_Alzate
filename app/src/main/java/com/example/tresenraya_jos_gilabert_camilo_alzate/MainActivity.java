fe8793ffa0496326d2f90494ce4bb2312e57f93e
tortmac1 <tortmacdelaletra@gmail.com>
2022-06-29 22:16:21 +0200

    Tres en raya, funcional tanto si gana el jugador como la IA, AC completada

--- app/src/main/java/com/example/tresenraya_jos_gilabert_camilo_alzate/MainActivity.java
+++ app/src/main/java/com/example/tresenraya_jos_gilabert_camilo_alzate/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tresenraya_jos_gilabert_camilo_alzate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     int estado = 0;
     int fichasPuestas = 0;
+    int turno =1;
+    int[] posGanadora = new int[]{-1,-1,-1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +46,13 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.casilla4), findViewById(R.id.casilla5), findViewById(R.id.casilla6),
                 findViewById(R.id.casilla7), findViewById(R.id.casilla8), findViewById(R.id.casilla9),
         };
-        /*
-        for (Button b: btns) {
-            b.setOnClickListener(view -> {
-               b.setText("X");
-            });
-        }
 
-         */
 
     }
 
     public void colocarFicha(View v){
         if(estado == 0){
+            turno = 1;
             int numBoton = Arrays.asList(botones).indexOf(v.getId());
             Button b = (Button) findViewById(botones[numBoton]);
             if(tablero[numBoton] == 0) {
@@ -63,10 +60,13 @@ public class MainActivity extends AppCompatActivity {
                 tablero[numBoton] = 1;
                 fichasPuestas += 1;
                 estado = comprobarEstado();
+                terminarPartida();
                 if (estado == 0) {
+                    turno = -1;
                     ia();
                     fichasPuestas += 1;
                     estado = comprobarEstado();
+                    terminarPartida();
                 }
             }
         }
@@ -86,11 +86,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public int comprobarEstado(){
-        if(fichasPuestas < 9){
-            return 0;
-        }
-        else{
-            return 2;
+      int nuevoEstado = 0;
+
+      if(Math.abs(tablero[0]+tablero[1]+tablero[2]) == 3){
+          posGanadora = new int[]{0,1,2};
+          nuevoEstado = 1*turno;
+      } else if (Math.abs(tablero[3]+tablero[4]+tablero[5]) == 3){
+          posGanadora = new int[]{3,4,5};
+          nuevoEstado = 1*turno;
+      }else if (Math.abs(tablero[6]+tablero[7]+tablero[8]) == 3){
+          posGanadora = new int[]{6,7,8};
+          nuevoEstado = 1*turno;
+      }else if (Math.abs(tablero[0]+tablero[3]+tablero[6]) == 3){
+          posGanadora = new int[]{0,3,6};
+          nuevoEstado = 1*turno;
+      }else if (Math.abs(tablero[1]+tablero[4]+tablero[7]) == 3){
+          posGanadora = new int[]{1,4,7};
+          nuevoEstado = 1*turno;
+      }else if (Math.abs(tablero[2]+tablero[5]+tablero[8]) == 3){
+          posGanadora = new int[]{2,5,8};
+          nuevoEstado = 1*turno;
+      }else if (Math.abs(tablero[0]+tablero[4]+tablero[8]) == 3){
+          posGanadora = new int[]{0,4,8};
+          nuevoEstado = 1*turno;
+      }else if (Math.abs(tablero[2]+tablero[4]+tablero[6]) == 3){
+          posGanadora = new int[]{2,4,6};
+          nuevoEstado = 1*turno;
+      }else if (fichasPuestas == 9){
+          nuevoEstado = 2;
+      }
+
+      return nuevoEstado;
+    }
+
+    public void terminarPartida(){
+        if(estado == 1 || estado == -1){
+            if (estado == 1){
+                textoVictoria.setVisibility(View.VISIBLE);
+                textoVictoria.setTextColor(Color.GREEN);
+            }else {
+                textoVictoria.setVisibility(View.VISIBLE);
+                textoVictoria.setText("Has perdido :c");
+                textoVictoria.setTextColor(Color.RED);
+            }
+
+        }else if (estado == 2){
+            textoVictoria.setVisibility(View.VISIBLE);
+            textoVictoria.setText("Has empatado :/");
+            textoVictoria.setTextColor(Color.YELLOW);
         }
     }
 }
